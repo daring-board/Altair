8ec2376d419b9425ba7475ed2a263828737d116b
daring-board <mahotox101500@gmail.com>
2019-03-17 17:53:07 +0900

    new Application commit

---
+++ backend/run.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, jsonify, make_response
+from random import *
+from flask_cors import CORS 
+
+app = Flask(__name__,
+            static_folder = "../dist/static",
+            template_folder = "../dist")
+CORS(app)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
+
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber': randint(1, 100)
+    }
+    return jsonify(response)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == "__main__":
+    print(" * Flask starting server...")
+    app.run(host='0.0.0.0',port=5000)
